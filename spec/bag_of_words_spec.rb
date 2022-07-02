f419b20fa63f4a6efcab4ced584fe421e2f7d935
Laylla Rodrigues <62724150+LayllaRodrigues@users.noreply.github.com>
2022-07-02 16:35:32 -0300

    refatorando para melhorar a assercao

--- spec/bag_of_words_spec.rb
+++ spec/bag_of_words_spec.rb
@@ -5,6 +5,6 @@ describe BagOfWords do
   it "is possible to put words on it" do
     bag = BagOfWords.new
     bag.put("hello", "world")
-    expect(bag.words.size).to eq(2)
+    expect(bag).to have(2).words    
   end
 end

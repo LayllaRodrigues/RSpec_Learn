53efd463df9e0b3a4ca848cd3870ad297cfe197a
Laylla Rodrigues <62724150+LayllaRodrigues@users.noreply.github.com>
2022-07-02 16:51:39 -0300

    adc expect have e gem para que ela possa ser usada

--- bag_of_words/lib/bag_of_words.rb
+++ bag_of_words/lib/bag_of_words.rb
@@ -1,11 +1,11 @@
 class BagOfWords
-    attr_reader :words
-  
-    def initialize
-      @words = []
-    end
-  
-    def put(*words)
-      # TODO
-    end
+  attr_reader :words
+
+  def initialize
+    @words = []
   end
+
+  def put(*words)
+    @words += words
+  end
+end

6f415ec8afa34189707042c9afe0167e5728bf23
Laylla Rodrigues <62724150+LayllaRodrigues@users.noreply.github.com>
2022-07-03 09:36:00 -0300

    add         lib/subcategory.rb

---
+++ RSpec_learn/lib/subcategory.rb
@@ -0,0 +1,18 @@
+require_relative "product"
+
+class Subcategory
+  attr_reader :name, :product
+
+  def initialize(name)
+    @name = name
+    @product
+  end
+
+  def add_product(product_name)
+    @product = Product.new(product_name)
+  end
+
+  def product
+    @product
+  end
+end

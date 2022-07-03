5e48697dc61a6b814a4d8bf6f52f999f4d44795a
Laylla Rodrigues <62724150+LayllaRodrigues@users.noreply.github.com>
2022-07-03 09:37:38 -0300

    spec/user_spec.rb

---
+++ RSpec_learn/spec/user_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe "User Spec" do
+  it "Should NOT save User with invalid attributes" do
+    expect {
+      invalid_attributes = { name: nil }
+      user = User.new(invalid_attributes)
+      user.save
+    }.to_not change(User, :instance_variables)
+  end
+end

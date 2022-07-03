b5d13eb22433a7e8b6b8c09c8b991b6f2486c0af
Laylla Rodrigues <62724150+LayllaRodrigues@users.noreply.github.com>
2022-07-03 09:37:25 -0300

    add spec/loan_checker_spec.rb

---
+++ RSpec_learn/spec/loan_checker_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe "Stubs Matchers" do
+  describe "LoanChecker Example" do
+    # Testa se um cliente pode fazer um empréstimo
+    describe "#can_lend_to?" do
+      it "returns true when the client has no debt" do
+        client = Client.new
+        allow(client).to receive(:has_debt?).and_return(false)
+
+        loan_checker = LoanChecker.new
+        loan_evaluation = loan_checker.can_lend_to?(client)
+        expect(loan_evaluation).to be_truthy
+      end
+    end
+  end
+end

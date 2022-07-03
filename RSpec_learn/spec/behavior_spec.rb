6b930764f521bd5159ea9d0b93cf8791afea9860
Laylla Rodrigues <62724150+LayllaRodrigues@users.noreply.github.com>
2022-07-02 21:47:53 -0300

    incluindo matcher behavior

---
+++ RSpec_learn/spec/behavior_spec.rb
@@ -0,0 +1,23 @@
+class DeepThought
+	def initialize(printer)
+		@printer = printer
+	end
+
+	def print_the_answer
+		@printer.print("42")
+	end
+end
+
+RSpec.describe DeepThought do
+  it "prints the answear to the ultimate question" do
+    printer = double("printer")
+    computer = DeepThought.new(printer)
+
+    # programa o objeto printer para verificar se o método print
+    # será chamado nesse objeto com o argumento "42" durante o
+    # teste
+    expect(printer).to receive(:print).with("42")
+
+    computer.print_the_answer
+  end
+end

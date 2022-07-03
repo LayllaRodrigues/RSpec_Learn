09d4c22d4c8a7d489c8ff7de076c42077366e19d
Laylla Rodrigues <62724150+LayllaRodrigues@users.noreply.github.com>
2022-07-03 09:43:32 -0300

    refactoring

--- RSpec_learn/spec/string_spec.rb
+++ RSpec_learn/spec/string_spec.rb
@@ -1,40 +1,40 @@
 RSpec.describe "String Matchers Examples" do
-  string = "gabriel schiavo"
+  string = "Laylla Rodrigues"
 
   it "Should match with regex" do
-    expect(string).to match(/gabriel/)
-    expect(string).to match(/chiav/)
+    expect(string).to match(/Laylla/)
+    expect(string).to match(/Rodrigues/)
   end
 
   it "Should NOT match with regex" do
-    expect(string).not_to match(/silva/)
-    expect(string).not_to match(/rafael/)
+    expect(string).not_to match(/Maia/)
+    expect(string).not_to match(/Reis/)
   end
 
   it "Should include some predefined strings or letters" do
-    expect(string).to include("g")
-    expect(string).to include("gabriel")
-    expect(string).to include("gab", "sch")
+    expect(string).to include("L")
+    expect(string).to include("Laylla")
+    expect(string).to include("Lay", "Rod")
   end
 
   it "Should NOT include some predefined strings" do
-    expect(string).not_to include("silva")
-    expect(string).not_to include("gabs")
+    expect(string).not_to include("batista")
+    expect(string).not_to include("baus")
   end
 
   it "Should start with my own first name" do
-    expect(string).to start_with("gabriel")
+    expect(string).to start_with("Laylla")
   end
 
   it "Should NOT START with my last name" do
-    expect(string).not_to start_with("schiavo")
+    expect(string).not_to start_with("Rodrigues")
   end
 
   it "Should end with my last name" do
-    expect(string).to end_with("schiavo")
+    expect(string).to end_with("Rodrigues")
   end
 
   it "Should NOT END with my first name" do
-    expect(string).not_to end_with("gabriel")
+    expect(string).not_to end_with("Laylla")
   end
 end

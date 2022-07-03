7483ddc54b7c80af713c0b43515654841178a378
Laylla Rodrigues <62724150+LayllaRodrigues@users.noreply.github.com>
2022-07-02 21:53:07 -0300

    incluindo matcher range

---
+++ RSpec_learn/spec/range_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe "Range Matchers unit" do
+  it "Should accept some intervals range value" do
+    range = (1..10)
+
+    expect(range).to cover(1)
+    expect(range).to cover(10)
+    expect(range).to cover(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    expect(range).to cover(5, 6, 7, 8)
+  end
+end
